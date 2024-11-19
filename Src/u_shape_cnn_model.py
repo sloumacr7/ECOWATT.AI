759bd03bf09617df8f5be5a5081458694960abf0
sloumacr7 <abdessalem.fakhar@insat.ucar.tn>
2024-11-19 15:31:44 +0100

    Update u_shape_cnn_model.py

--- Src/u_shape_cnn_model.py
+++ Src/u_shape_cnn_model.py
@@ -1,13 +1,1 @@
-import pickle
 
-# Load model
-MODEL_PATH = "./src/u_shape_cnn_model.pkl"
-
-with open(MODEL_PATH, 'rb') as file:
-    model = pickle.load(file)
-
-# Example usage
-def predict(input_data):
-    # Ensure input_data is preprocessed as needed
-    predictions = model.predict(input_data)
-    return predictions
