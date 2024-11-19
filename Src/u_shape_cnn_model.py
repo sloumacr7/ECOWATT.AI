f75d9299cd32d2e125a90cb9f3d64a0c8798596e
sloumacr7 <abdessalem.fakhar@insat.ucar.tn>
2024-11-19 10:37:39 +0100

    Update u_shape_cnn_model.py

--- Src/u_shape_cnn_model.py
+++ Src/u_shape_cnn_model.py
@@ -1,7 +1,7 @@
 import pickle
 
 # Load model
-MODEL_PATH = "./models/u_shape_cnn_model.pkl"
+MODEL_PATH = "./src/u_shape_cnn_model.pkl"
 
 with open(MODEL_PATH, 'rb') as file:
     model = pickle.load(file)
